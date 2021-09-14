2dbd3254b4e6321d5fdb88c2799c3e2fcaf7d07d
mwkmwkmwk <mwkmwkmwk@users.noreply.github.com>
2021-09-14 01:39:04 +0000

    deploy: ccf4b52478b27a48d9e755dde3bf8bd080cfc1d8

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["vfront_basics"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"vfront_basics/diag/enum.DiagSeverity.html\" title=\"enum vfront_basics::diag::DiagSeverity\">DiagSeverity</a>","synthetic":false,"types":["vfront_basics::diag::DiagSeverity"]}];
+implementors["vfront_basics"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"vfront_basics/diag/enum.DiagSeverity.html\" title=\"enum vfront_basics::diag::DiagSeverity\">DiagSeverity</a>","synthetic":false,"types":["vfront_basics::diag::DiagSeverity"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
