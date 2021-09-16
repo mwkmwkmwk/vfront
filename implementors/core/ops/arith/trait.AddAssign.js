2a6856cdf9d6b4e61d00a205451f0f93b6a6f682
mwkmwkmwk <mwkmwkmwk@users.noreply.github.com>
2021-09-16 18:35:57 +0000

    deploy: b361fb3d2eaf71119b612f6aced81c182954c205

---
+++ implementors/core/ops/arith/trait.AddAssign.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["vfront_basics"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"vfront_basics/source/struct.SourceRef.html\" title=\"struct vfront_basics::source::SourceRef\">SourceRef</a>&lt;'a&gt;","synthetic":false,"types":["vfront_basics::source::SourceRef"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
